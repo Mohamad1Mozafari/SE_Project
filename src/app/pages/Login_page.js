f166e7ffb171347e59b08df7742d9cb4ad606669
Mohamad <dr.m.m.tesla@gmail.com>
2026-06-18 16:39:16 +0330

    this is for login.tsx combine to js

---
+++ src/app/pages/Login_page.js
@@ -0,0 +1,6 @@
+export default function login_handler (username , password ){
+// console.log (username); 
+// console.log (password);
+const result = "this user with username :"+ username + "and this password "+password +"is login ";
+return result ;
+}
