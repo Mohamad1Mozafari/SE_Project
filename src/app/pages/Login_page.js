3245305f0fb2e8a5ad46d0cac91e15713b252d4f
Mohamad1Mozafari <mohamad.mozafari.12212@gmail.com>
2026-06-28 13:30:35 +0330

    upload to hamgit

--- src/app/pages/Login_page.js
+++ src/app/pages/Login_page.js
@@ -1,9 +1,9 @@
-// export default function login_handler (username , password ){
-// // console.log (username); 
-// // console.log (password);
-// const result = "this user with username :"+ username + "and this password "+password +"is login ";
-// return result ;
-// }
+export default function login_handler (username , password ){
+// console.log (username); 
+// console.log (password);
+const result = "this user with username :"+ username + "and this password "+password +"is login ";
+return result ;
+}
 
 // const sql = require ('mssql');
 
