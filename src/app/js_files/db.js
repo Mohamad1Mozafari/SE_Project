adccca3077b817e719aea6085f4785a89fa3ed18
Ahmad-Mofti <ahmadmofti4@gmail.com>
2026-07-03 01:22:43 +0330

    Initialize SQL Server Database Connection

---
+++ src/app/js_files/db.js
@@ -0,0 +1,26 @@
+const sql = require("mssql");
+
+const config = {
+  user: "parking_user",
+  password: "12345",
+  server: ".",
+  database: "ParkingDB",
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+  },
+  port: 1433,
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then((pool) => {
+    console.log("Connected to the SQL Server (ParkingDB)");
+    return pool;
+  })
+  .catch((err) => {
+    console.error("can not connect to the Database!", err.message);
+    process.exit(1);
+  });
+
+module.exports = { sql, poolPromise };
